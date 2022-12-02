8e82ca3a7c1e1d67413c522acb5a8a50317a3ecb
noah102 <noah@kohrs.fr>
2022-12-02 04:37:31 +0100

    Ajout images (non implémentés)

--- typescript/images.ts
+++ typescript/images.ts
@@ -1,3 +1,15 @@
 export const images: { [key: string]: string } = {
+    "Dance": "img/Dance.jpg",
+    "Arrivee": "img/Arrivee.jpg",
+    "Bar": "img/Bar.jpg",
+    "Chambre": "img/Chambre.jpg",
+    "Dehors": "img/Dehors.jpg",
+    "Embrasser": "img/Embrasser.jpg",
+    "Maladies": "img/Maladies.jpg",
+    "Partouze": "img/Partouze.jpg",
+    "Salon": "img/Salon.jpg",
+    "Se_Faire_Test": "img/Se_Faire_Tester.jpg",
+    "ShiFuBois": "img/ShiFuBois.jpg",
+    // Background Image
     
 }
